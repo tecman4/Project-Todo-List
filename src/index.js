47711afb7619f755a5d06d4abebf1711c92f9db0
Wayne <tecman4@gmail.com>
2024-08-28 16:49:00 -0400

    Still working on Javascript

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 let content = document.querySelector("#content")
-content.innerHTML= "<div>Welcome to Wayne's Home Diner</div>";
-
-
-
-
-const value = document.querySelector("#value");
-const input = document.querySelector("#priority");
+let value = document.querySelector("#value");
+let input = document.querySelector("#priority");
 value.textContent = input.value;
 input.addEventListener("input", (event) => {
   value.textContent = event.target.value;
+
+
 });
 
 function newTODO(){
 
-}
+  content.innerHTML= title.value
+  return;
+  }
